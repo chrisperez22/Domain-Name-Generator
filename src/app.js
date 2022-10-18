ff204d2e49beeab8b0e5908d953778ce052eb259
chrisperez22 <chrisperez0531@gmail.com>
2022-10-18 05:57:55 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// import { Generator } from "webpack";
 
+let pronoun = ["The", "Our"];
+let adj = ["great", "big", "huge"];
+let noun = ["lion", "racoon", "tiger"];
+let ext = [".com", ".net", ".gov"];
+
+function randomgenerator(multiply) {
+  return Math.floor(Math.random() * multiply.length);
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML =
+    pronoun[randomgenerator(pronoun)] +
+    " " +
+    adj[randomgenerator(adj)] +
+    " " +
+    noun[randomgenerator(noun)] +
+    "" +
+    ext[randomgenerator(ext)];
+
+  console.log("Hello Rigo!");
 };
